48cae532ad02c02dc862121832cc274549d671ca
Walter Cordoba <walterflycba@gmail.com>
2022-09-05 17:47:41 -0300

    Cambios Estilo

--- src/App.js
+++ src/App.js
@@ -25,10 +25,7 @@ function App() {
   return (
     <div className='App'>
       <div className='freecodecamp-logo-contenedor'>
-        <img 
-          src={freeCodeCampLogo}
-          className='freecodecamp-logo'
-          alt='Logo de freeCodeCamp' />
+        
       </div>
       <div className='contenedor-calculadora'>
         <Pantalla input={input}/>
